e0daca6c7796ffd2fcc36a804b69a964fe7c3d90
Vinobharathi Parthasarathy <vinobharathipkavitha@gmail.com>
2025-07-31 15:55:45 +0000

    background image in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,10 +7,10 @@ export default function LoginPage() {
        <div 
         className="absolute inset-0 bg-cover bg-center z-0" 
         style={{ 
-          backgroundImage: "url('/background.png')",
+          backgroundImage: "url('https://images.unsplash.com/photo-1690855214743-9a78bc383669?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
           filter: 'blur(2px) brightness(0.6)'
         }}
-        data-ai-hint="hyundai car night city"
+        data-ai-hint="hyundai car studio"
       ></div>
       <div className="relative z-10 grid md:grid-cols-2 gap-16 items-center max-w-6xl mx-auto">
         <div className="text-white space-y-4">
